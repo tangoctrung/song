7f373ff5191fbace4dde001d0c725b437880fc46
tangoctrung <tangoctrung12123@gmail.com>
2024-07-31 08:52:02 +0700

    build ui hom

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_example/data/repository/repository.dart';
+import 'package:flutter_example/ui/discovery/discovery.dart';
+import 'package:flutter_example/ui/home/home.dart';
+import 'package:flutter_example/ui/setting/setting.dart';
+import 'package:flutter_example/ui/user/user.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  var repository = DefaultRepository();
-  var songs = await repository.loadData();
-  if (songs != null) {
-    for (var song in songs) {
-      debugPrint(song.toString());
-      print(song.title);
-    }
-  }
-  // runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,60 +15,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Song app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MySongApp(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MySongApp extends StatefulWidget {
+  const MySongApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MySongApp> createState() => _MySongAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MySongAppState extends State<MySongApp> {
+  final List<Widget> _tabs = [
+    const HomeTab(),
+    const DiscoveryTab(),
+    const AccountTab(),
+    const SettingTab(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text("Song App"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      child: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.album), label: "Discovery"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Setting"),
           ],
         ),
+        tabBuilder: (BuildContext context, int index) {
+          return _tabs[index];
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/ui/home/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Home Tab"),
+      ),
+    );
+  }
+}
