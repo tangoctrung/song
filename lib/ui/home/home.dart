e0da40cd6643a06d80dc7da982fc78c4e4df3d85
tangoctrung <tangoctrung12123@gmail.com>
2024-07-31 11:01:59 +0700

    change name, logo app

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -5,10 +5,20 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Tab"),
-      ),
-    );
+    return const HomeTabPage();
+  }
+}
+
+class HomeTabPage extends StatefulWidget {
+  const HomeTabPage({super.key});
+
+  @override
+  State<HomeTabPage> createState() => _HomeTabPageState();
+}
+
+class _HomeTabPageState extends State<HomeTabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
